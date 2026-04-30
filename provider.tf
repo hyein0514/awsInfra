aa4f7120015d10e805b196b7c578f8f530b58fe2
Hyein <hi020514@yourdomain.com>
2026-04-30 09:54:54 +0900

    S3부분 삭제

--- provider.tf
+++ provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-
-  backend "s3" {
-    bucket         = "myapp-terraform-state-20260429-hyen"
-    key            = "training/lab06/terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
