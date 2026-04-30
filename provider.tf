622173e54a32778487f283c0928dfd0784800a10
Hyein <hi020514@yourdomain.com>
2026-04-30 09:43:46 +0900

    region change

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "myapp-terraform-state-20260429"
+    bucket         = "myapp-terraform-state-20260429-hyen"
     key            = "training/lab06/terraform.tfstate"
-    region         = "ap-northeast-2"
+    region         = "ap-southeast-2"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
